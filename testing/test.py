72aa58bb9501053115858795d726eae8ec206347
ekmb <ebakhturina@nvidia.com>
2021-06-22 07:33:45 -0700

    ru e replacement, make pre-req added
    Signed-off-by: ekmb <ebakhturina@nvidia.com>

--- testing/test.py
+++ testing/test.py
@@ -6,7 +6,6 @@ grammar_dir = '/home/ebakhturina/itn_cg/TextNormalizationCoveringGrammars/src'
 eng_final_fst = pynini.Far(f'{grammar_dir}/en/verbalizer/verbalizer.far', mode='r')['VERBALIZER']
 ru_final_fst = pynini.Far(f'{grammar_dir}/ru/verbalizer/verbalizer.far', mode='r')['VERBALIZER']
 ru_numbers_fst = pynini.Far(f'{grammar_dir}/ru/verbalizer/numbers.far', mode='r')['CARDINAL_DEFAULT']
-other_numbers_fst = pynini.Far(f'{grammar_dir}/other/verbalizer/numbers.far', mode='r')['CARDINAL_DEFAULT']
 
 example = {'tn': "23", 'itn': "двадцать восемь"}
 print('NORMALIZATION:')
@@ -17,4 +16,13 @@ print('-' * 60)
 print('INVERSE NORMALIZATION:')
 invert = ru_numbers_fst.invert().optimize()
 print('RU:', rewrite.top_rewrite(example['itn'], invert))
-print('Other:', rewrite.top_rewrite(example['itn'], other_numbers_fst.invert().optimize()))
+
+try:
+    other_numbers_fst = pynini.Far(f'{grammar_dir}/other/verbalizer/numbers.far', mode='r')['CARDINAL_DEFAULT']
+    print('Other:', rewrite.top_rewrite(example['itn'], other_numbers_fst.invert().optimize()))
+except:
+    print('grammar for other language is not found')
+
+print("трехсот трех" in rewrite.rewrites("303", ru_final_fst))
+import pdb; pdb.set_trace()
+print()
